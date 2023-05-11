41a6d268eae8839d0a65cbfd911cda92add7e08c
Mujaahid <slxmuj002@myuct.ac.za>
2023-05-11 21:55:15 +0200

    TODO - Apache POI

---
+++ src/LoadTester.java
@@ -0,0 +1,21 @@
+// import java.util.Scanner;
+// import org.apache.jmeter.engine.StandardJMeterEngine;
+// import org.apache.jmeter.testelement.TestPlan;
+// import org.apache.jmeter.save.SaveService;
+// import org.apache.jmeter.util.JMeterUtils;
+// import org.apache.jmeter.control.LoopController;
+// import org.apache.jmeter.threads.ThreadGroup;
+// import org.apache.jmeter.protocol.http.sampler.HTTPSampler;
+// import org.apache.jmeter.config.Arguments;
+// import org.apache.jmeter.reporters.ResultCollector;
+// import org.apache.jmeter.report.dashboard.ReportGenerator;
+
+
+public class LoadTester 
+{
+
+
+
+
+
+}
